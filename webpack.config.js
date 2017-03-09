0b57de9a38f3618e28907c93b8a5774afebe2307
taiyuan321@163.com <taiyuan321@163.com>
2017-03-09 21:33:17 +0800

    ficxbug

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require("webpack");
 var path = require('path');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
-let extractCSS = new ExtractTextPlugin('/css/[name].css');
+var extractCSS = new ExtractTextPlugin('/css/[name].css');
 
 module.exports = {
     entry: {
